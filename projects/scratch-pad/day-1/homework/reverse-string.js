d54b6ae0f0136ec1a57254465c9a71c37d28d601
Ubuntu <ubuntu@ip-172-31-92-14.ec2.internal>
2019-12-10 05:29:13 +0000

    scratch pad done

---
+++ projects/scratch-pad/day-1/homework/reverse-string.js
@@ -0,0 +1,52 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 1: reverseString()
+ *
+ *  a. Create a function called reverseString that takes a 
+ *  String as its only input and returns a new String 
+ *  representing the input String reversed.  For example:
+ *  
+ *      reverseString('hello');  // => 'olleh'
+ * 
+ * TIPS:
+ *  a. create something to collect the output you'll return.
+ *  b. you'll need a loop, which one is best? How do you know 
+ *     when to stop looping?
+ *  c. how do you access individual characters of a String?
+ *  d. how do you concatenate Strings? What operator do we use?
+ */
+ 
+/* creating a loop to reverse characters in a string and produce a new string
+start condition: end of string
+stop condition: beginning of string
+decrease by one and concatenate a new string 
+*/
+
+function reverseString(input) {
+    // YOUR CODE GOES BELOW HERE //
+        var reversed = "";
+        for (var i = input.length-1; i >= 0; i --) {
+          reversed += input[i];
+          
+        } return reversed;
+        
+    
+    
+    
+    // YOUR CODE GOES ABOVE HERE //
+}
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.reverseString = reverseString;
+}
