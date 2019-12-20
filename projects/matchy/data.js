be6ceab3a0613acd86bb426ac537f1ec609e3f9b
Ubuntu <ubuntu@ip-172-31-92-14.ec2.internal>
2019-12-20 00:38:50 +0000

    matchy update

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,28 +11,62 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/**
+ * created an object literal assigned a variable animal;
+ * added a species key to animal with a value of 'dog' using dot notation;
+ * added a name key to animal with a value of 'gonzo' using bracket notation;
+ * added a noises key to animal using dot notation then assigned it to an array;
+ * ran console.log on animal
+ */
 
-
-
+var animal = {};
+animal.species ='dog'; 
+animal['name'] = 'gonzo';
+animal.noises = [];
+console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * created a variable named noises and assigned it to an empty array;
+ * assigned noises array an element of 'bark' at its first index;
+ * assigned a value of 'woof' to the last index of noises array;
+ * added a value of 'meep' to beginning of noises array;
+ * used .push() to add 'squeak' to end of noises array;
+ * used noises.length to console.log length of array;
+ * accessed value of last index of noises array with bracket notation around noises.length;
+ * printed noises to console;
+ */
+ 
+var noises = [];
+noises[0] = 'bark';
+noises[noises.length] = 'woof';
+noises.unshift('meep');
+noises.push('squeak');
+console.log(noises.length);
+console.log(noises[noises.length-1]);
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * added a noises property to animal using bracket notation;
+ * used .push with dot notation to add 'squeak' to noises property in animal
+ * printed animal to console
+ */
+ 
+animal['noises'] = noises;
+animal.noises.push('squeak');
+console.log(animal);
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ * You can use dot notation and bracket notation.
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * You can use bracket notaion.
  * *******************************************************************
  */
 
@@ -48,14 +82,62 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * created an animals variable and assigned it to empty array;
+ * used .push() to assign animal to animals array;
+ * printed animals to console;
+ * create a duck object and assign it key/value pairs provided;
+ * used .push() to add duck to animals array;
+ * printed to console animals;
+ * created two more objects(cat, bird) with key/value pairs;
+ * printed to console animals and the length of animals;
+ */
+ 
+ var animals = [];
+ animals.push(animal);
+ console.log(animals);
+ var duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+ animals.push(duck);
+ console.log(animals);
+ var cat = { 
+     species: 'feline', 
+     name: 'monk', 
+     noises: ['meow', 'purr', 'sneeze', 'woosh'] };
+     animals.push(cat);
+ var bird = { 
+     species: 'bird',
+     name: 'tweety', 
+     noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+ animals.push(bird);
+console.log(animals);
+console.log(animals.length);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * created a variable friends and assigned it to an empty array;
+ * ceated a 'getRandomInt' function that will find a random integer;
+ * Wrote a function called 'getRandom' that takes our `animals` array and returns the `index` of a random element using `Math.random`
+ * used 'getRandomInt' within 'getRandom'
+ * used .push() with 'getRandom' function to find a random animal from array then used .name to access the name and then push the name;
+ * print friends to console;
+ * used 'getRandom' on animals to find a random animal, followed by .friends to add as a friends property and then assigned that to 'friends';
+ * printed work to console;
+ */
+ 
+var friends = [];
+// I chose an array to make a list of friends because it can hold a collection.
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function getRandom(animals){
+    return animals[getRandomInt(animals.length -1)];
+}
+friends.push(getRandom(animals).name);
+console.log(friends);
+getRandom(animals).friends = friends;
+console.log(animals);
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,26 +12,77 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/**
+ * Input: animals array and string(name);
+ * Output: one of the animals from array; null if there is no match;
+ * constraints: return animal if it exists in animasl array;
+ * edge case: use bracket notation
+ */
+ 
+function search(animals, name) {
+   
+    for (var i = 0; i < animals.length; i++) {
+        if(name === animals[i].name) {
+            return animals[i];
+        } 
+      }     return null;
+    }
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/**
+ * Input: animals array, animal name, replacement name;
+ * Output: replacement name
+ * Constraints: if animal name matches a name in animals array replace it with replacement name;
+ * Edge Case: use .splice() method to alter animals array with new name;
+ */
+function replace(animals, name, replacement) {
+     for (var i = 0; i < animals.length; i++) {
+         if (name === animals[i].name) {
+             return animals.splice(animals[i], 1, replacement);
+         }
+     }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/**
+ * input: animals array, animal name
+ * output: animals array with name removed
+ * constraints: if an animal name matches one in the animals array remove it;
+ * edge case: use .splice() to return altered array;
+ */
+ 
+function remove(animals, name) {
+    for(var i =0; i< animals.length; i++) {
+        if (name === animals[i].name) {
+            return animals.splice(animals[i],1);
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Create ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * input: animals array, animal name;
+ * output: array with new animal added to it;
+ * constraints: make sure animal name property has a value > 0, make sure animal species property has a value greater than 0, make sure
+ * the animal name does not match any other name in animals array;
+ * edge case: use .some() to check if animal name is the same as a name already in animals array;
+ */
+ 
+function add(animals, animal) {
+   if(animal.species.length > 0 && animal.name.length > 0 && !animals.some((element) => (element.name === animal.name))) {
+           return animals.push(animal);
+           }
+        }
 
 /**
  * You did it! You're all done with Matchy!
